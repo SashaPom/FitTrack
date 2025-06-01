35d1c702f7006404a1ae1169d0672bc037dc1439
sashapom <pomazansanya2004@gmail.com>
2025-06-01 22:43:39 +0300

    Add security config for jwt auth with roles

---
+++ src/main/java/com/duikt/fittrack/config/security/role/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.duikt.fittrack.config.security.role;
+
+import com.duikt.fittrack.entity.UserEntity;
+import com.duikt.fittrack.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Could not find user: " + email));
+
+        return new UserDetailsImpl(user);
+    }
+
+}
